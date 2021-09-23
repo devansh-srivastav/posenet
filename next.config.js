1bba4e1d19aeaa6ccc666ff9cc8b7a77886ddf3e
Devansh Srivastav <devansh.srivastav@gmail.com>
2021-09-23 16:40:33 +0530

    posenet

--- next.config.js
+++ next.config.js
@@ -1,27 +1,8 @@
-// This file was automatically added by layer0 init.
-// You should commit this file to source control.
-// const { withLayer0, withServiceWorker } = require('@layer0/next/config')
-
-// This file was automatically added by layer0 init.
-// You should commit this file to source control.
 const { withLayer0, withServiceWorker } = require('@layer0/next/config')
 
-const _preLayer0Export = (phase, config) =>
-  withLayer0(
-    withServiceWorker({
-      // Output sourcemaps so that stack traces have original source filenames and line numbers when tailing
-      // the logs in the Layer0 developer console.
-      layer0SourceMaps: true,
-    })
-  );;
-
-module.exports = (phase, config) =>
-  withLayer0(
-    withServiceWorker({
-      // Output sourcemaps so that stack traces have original source filenames and line numbers when tailing
-      // the logs in the Layer0 developer console.
-      layer0SourceMaps: true,
-
-      ..._preLayer0Export(phase, config)
-    })
-  )
+module.exports = withLayer0(
+  withServiceWorker({
+    layer0SourceMaps: true,
+    reactStrictMode: true,
+  })
+)
